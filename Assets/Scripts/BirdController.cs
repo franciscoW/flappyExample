4c1378b12b3ae632d3c42cd5d0b7754b94e07415
miguel <franc319@gmail.com>
2017-08-25 08:51:38 -0500

    new project config
    new project config

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	private bool isDead = false;
+	private bool isFirstTimeDeath = true;
+	private Rigidbody2D rgd2d;
+	private float maxAltura	= 5.2f;
+
+	//variables publicas
+	public float upForce = 200f;
+	public GameObject bird;
+	public Animator animator;
+
+	void Awake(){
+		rgd2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isDead == false) {
+			if (Input.GetMouseButtonDown (0) && bird.transform.position.y < maxAltura) {
+				rgd2d.velocity = Vector2.zero;
+				rgd2d.AddForce (Vector2.up * upForce);
+				animator.SetTrigger ("Fly");
+				SoundSystem.instance.PlayTap ();
+				 
+			}//fin del if input
+		}//fin del if isDead.
+
+	}//fin del update
+
+
+	void OnCollisionEnter2D(Collision2D collesion){
+		isDead = true;
+		animator.SetTrigger ("Die");
+		rgd2d.velocity = Vector2.zero;
+		SoundSystem.instance.StopAudio ();
+		Invoke ("DelayRestartScene",1f);
+		if (isFirstTimeDeath) {
+			SoundSystem.instance.PlayDie ();
+			isFirstTimeDeath = false;
+		}
+			
+	}
+ 
+
+	private void DelayRestartScene(){
+		GameController.instance.BirdDie ();
+	}
+}//fin del component
---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColumnPool : MonoBehaviour {
+
+	public int ColumnPoollSize = 5 ;
+	private GameObject[] columns;
+	public GameObject columnPrefab;
+	private Vector2 objPoolPos	= new Vector2 (-11.46f, 1.92f);
+	private float timeSince ;
+	public float rateTimeCol;
+	public float colMinY = -0.37f;
+	public float colMaxY = 2.89f;
+	private float colPosX = 9.57f;
+	private int currCol;
+
+	// Use this for initialization
+	void Start () {
+
+		columns = new GameObject[ColumnPoollSize];
+
+		for(int i = 0 ; i < ColumnPoollSize; i++){
+			columns [i] =(GameObject) Instantiate (columnPrefab,objPoolPos,Quaternion.identity);
+		}
+		setColumn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeSince += Time.deltaTime; 
+
+		if( ! GameController.instance.gameOver &&  timeSince >= rateTimeCol){
+			 
+			timeSince = 0;
+			setColumn ();
+		}
+	}
+
+	void setColumn(){
+
+		float ranPosCol = Random.Range (colMinY, colMaxY);
+		columns [currCol].transform.position = new Vector2 (colPosX,ranPosCol);
+		currCol++;
+		if (currCol >= ColumnPoollSize) {
+			currCol = 0;
+		}
+	}
+}
